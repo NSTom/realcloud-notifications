8b81876169cfcf06d31e58e2ca38b5ec54ba7f97
Tom Freijsen <tomfreijsen@funda.nl>
2020-11-06 15:54:01 +0100

    Add function attribute to Firebase sink

--- Sinks/FirebaseSink.cs
+++ Sinks/FirebaseSink.cs
@@ -1,21 +1,13 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Storage;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Funda.RealCloudNotifications.Messaging;
-using Funda.RealCloudNotifications;
 
 [assembly: FunctionsStartup(typeof(Funda.RealCloudNotifications.Startup))]
 
 namespace Funda.RealCloudNotifications.Sinks
 {
-    public class FirebaseSink
+  public class FirebaseSink
     {
         private readonly IMessageOpener _messageOpener;
 
@@ -23,7 +15,8 @@ namespace Funda.RealCloudNotifications.Sinks
         {
             _messageOpener = messageOpener;
         }
-
+        
+        [FunctionName("FirebaseSink")]
         public void Run(
             [QueueTrigger("NotificationsQueue", Connection = "Endpoint=sb://sb-realcloud-notifications.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ezp+qCJlFA9c6lnxiX3sVyvOwadx+PpHdytpVGCQhyc=")] string unopenedMessage, 
             ILogger log)
