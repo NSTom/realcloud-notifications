4e199736a7d4b901b539ce8f824d96ad0b42f79b
Tom Freijsen <tomfreijsen@funda.nl>
2020-11-06 16:33:25 +0100

    Fix connection string

--- Sinks/FirebaseSink.cs
+++ Sinks/FirebaseSink.cs
@@ -2,12 +2,13 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Funda.RealCloudNotifications.Messaging;
+using System;
 
 [assembly: FunctionsStartup(typeof(Funda.RealCloudNotifications.Startup))]
 
 namespace Funda.RealCloudNotifications.Sinks
 {
-  public class FirebaseSink
+    public class FirebaseSink
     {
         private readonly IMessageOpener _messageOpener;
 
@@ -18,7 +19,11 @@ namespace Funda.RealCloudNotifications.Sinks
         
         [FunctionName("FirebaseSink")]
         public void Run(
-            [QueueTrigger("NotificationsQueue", Connection = "Endpoint=sb://sb-realcloud-notifications.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ezp+qCJlFA9c6lnxiX3sVyvOwadx+PpHdytpVGCQhyc=")] string unopenedMessage, 
+            [ServiceBusTrigger("NotificationsQueue", Connection = "ServiceBusConnectionString")]
+            string unopenedMessage, 
+            Int32 deliveryCount,
+            DateTime enqueuedTimeUtc,
+            string messageId,
             ILogger log)
         {
             // Open the message
