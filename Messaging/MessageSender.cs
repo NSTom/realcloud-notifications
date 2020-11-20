7d4d7cf09b3f53c9ab3f47d8a142eee26aac2c97
Tom Freijsen <tomfreijsen@funda.nl>
2020-11-20 15:18:20 +0100

    Use connection string from app settings for sending

--- Messaging/MessageSender.cs
+++ Messaging/MessageSender.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 
 [assembly: FunctionsStartup(typeof(Funda.RealCloudNotifications.Startup))]
 
@@ -11,14 +12,20 @@ namespace Funda.RealCloudNotifications.Messaging
 {
     public class MessageSender : IMessageSender
     {
+        private readonly string _serviceBusConnectionString;
+
+        public MessageSender(IConfiguration configuration)
+        {
+            _serviceBusConnectionString = configuration["ServiceBusConnectionString"];
+        }
+
         public async Task SendAsync(IMessage message, string queueName)
         {
             // Encode to json
             var messageType = message.GetType();
             var json = JsonSerializer.Serialize(message, messageType);
 
-            var connectionString = "Endpoint=sb://sb-realcloud-notifications.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ezp+qCJlFA9c6lnxiX3sVyvOwadx+PpHdytpVGCQhyc=";
-            var queueClient = new QueueClient(connectionString, queueName);
+            var queueClient = new QueueClient(_serviceBusConnectionString, queueName);
 
             await queueClient.SendAsync(new Message(Encoding.UTF8.GetBytes(json)));
         }
